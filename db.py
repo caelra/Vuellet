c525870c13af99206697b19ad7ac2f50528e9902
Carlos Ramos <caelra99@gmail.com>
2020-06-01 23:02:23 -0500

    improve db

--- db.py
+++ db.py
@@ -1,61 +1,54 @@
 import sqlite3
 from sqlite3 import Error
 
-
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
-
-    return conn
-
-def create_table(conn, create_table_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
-
-def create_color(conn, color):
-    sql = ''' INSERT INTO colors(color) VALUES(?) '''
-    cur = conn.cursor()
-    cur.execute(sql, color)
-    return cur.lastrowid
-
-def select_all_colors(conn):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM colors")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-def main():
-    database = r"vuellet.db"
-
-    sql_create_colors_table = """ CREATE TABLE IF NOT EXISTS colors (
-                                      id integer PRIMARY KEY,
-                                      color text NOT NULL
-                                  );"""
-
-    # create a database connection
-    conn = create_connection(database)
-
-    # create tables
-    if conn is not None:
-        create_table(conn, sql_create_colors_table)
-        ## create a new project
-        # color = ('[color, text, array]')
-        # project_id = create_project(conn, color)
-        # print("=> Query all colors")
-        # select_all_colors(conn)
-    else:
-        print("Error! cannot create the database connection.")
+class DataBase:
+    def __init__(self):
+        self.db_file = r"vuellet.db"
+        self.conn = None
+        self.create_connection()
+        self.init_table()
 
 
-if __name__ == '__main__':
-    main()
+    def create_connection(self):
+        try:
+            self.conn = sqlite3.connect(self.db_file)
+            return self.conn
+        except Error as e:
+            print(e)
+
+        return self.conn
+
+    def create_table(self, conn, create_table_sql):
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
+
+    def create_color(self, colors):
+        sql = ''' INSERT INTO colors(color) VALUES(?) '''
+        cur = self.conn.cursor()
+        cur.execute(sql, (colors,))
+        self.conn.commit()
+        return cur.lastrowid
+
+    def select_all_colors(self):
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM colors")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    def init_table(self):
+        sql_create_colors_table = """ 
+            CREATE TABLE IF NOT EXISTS colors (
+                id integer PRIMARY KEY,
+                color text NOT NULL
+            );"""
+        if self.conn is not None:
+            self.create_table(self.conn, sql_create_colors_table)
+        else:
+            print("Error! cannot create the database connection.")
+
